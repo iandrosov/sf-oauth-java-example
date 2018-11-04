7f35f91397f711aa1fef423ca10f3ea35b2658f8
Igor <iandrosov73@gmail.com>
2018-11-04 07:28:20 -0500

    updates for oauth setup

--- src/main/java/com/example/Main.java
+++ src/main/java/com/example/Main.java
@@ -29,6 +29,12 @@ import org.springframework.context.annotation.Bean;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
 
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.ArrayList;
+import java.util.List;
+
 import javax.sql.DataSource;
 import java.sql.Connection;
 import java.sql.ResultSet;
@@ -41,6 +47,16 @@ import static javax.measure.unit.SI.KILOGRAM;
 import javax.measure.quantity.Mass;
 import org.jscience.physics.model.RelativisticModel;
 import org.jscience.physics.amount.Amount;
+
+import org.apache.http.HttpEntity;
+import org.apache.http.NameValuePair;
+import org.apache.http.client.entity.UrlEncodedFormEntity;
+import org.apache.http.client.methods.CloseableHttpResponse;
+import org.apache.http.client.methods.HttpGet;
+import org.apache.http.client.methods.HttpPost;
+import org.apache.http.impl.client.CloseableHttpClient;
+import org.apache.http.impl.client.HttpClients;
+import org.apache.http.message.BasicNameValuePair;
 
 @Controller
 @SpringBootApplication
@@ -59,6 +75,48 @@ public class Main {
   @RequestMapping("/")
   String index() {
     return "index";
+  }
+
+  @RequestMapping("/sfauth")
+  public String sfoauth() throws IOException {
+/*
+    initParams = { 
+    @WebInitParam(name = "clientId", value = "3MVG9yZ.WNe6byQDinV4pEtYbk.XKrK3LwCNZtKCJ9lKnd6keoaNjuNXu7i3EBK_lLzNSZnXAkQE.2gw4xFZn"),
+    @WebInitParam(name = "clientSecret", value = "8219049706333485472"),
+    @WebInitParam(name = "redirectUri", value = "https://localhost:5000/oauth/_callback"),
+    @WebInitParam(name = "environment", value = "https://login.salesforce.com/services/oauth2/token")  }
+ 
+    HttpClient httpclient = new HttpClient();
+    PostMethod post = new PostMethod(environment);
+    post.addParameter("code",code);
+    post.addParameter("grant_type","authorization_code");
+
+   ** For session ID instead of OAuth 2.0, use "grant_type", "password" **
+    post.addParameter("client_id",clientId);
+    post.addParameter("client_secret",clientSecret);
+    post.addParameter("redirect_uri",redirectUri);  
+
+    HttpResponse response = httpclient.execute(post);
+**/
+
+    CloseableHttpClient client = HttpClients.createDefault();
+    HttpPost httpPost = new HttpPost("https://login.salesforce.com/services/oauth2/authorize");
+ 
+    List<NameValuePair> params = new ArrayList<NameValuePair>();
+    params.add(new BasicNameValuePair("grant_type", "authorization_code"));
+    //params.add(new BasicNameValuePair("response_type", "code"));
+    params.add(new BasicNameValuePair("client_id", "3MVG9yZ.WNe6byQDinV4pEtYbk.XKrK3LwCNZtKCJ9lKnd6keoaNjuNXu7i3EBK_lLzNSZnXAkQE.2gw4xFZn"));
+    params.add(new BasicNameValuePair("client_secret", "8219049706333485472"));
+    params.add(new BasicNameValuePair("redirect_uri", "https://localhost:5000/oauth/_callback"));
+    
+    httpPost.setEntity(new UrlEncodedFormEntity(params));
+ 
+    CloseableHttpResponse response = client.execute(httpPost);
+    //assertThat(response.getStatusLine().getStatusCode(), equalTo(200));
+    client.close();
+
+    System.out.println("### HTTP: "+response);
+    return "hello";
   }
 
   @RequestMapping("/oauth/_callback")
